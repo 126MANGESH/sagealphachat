c921fa89c0b1f3adab30006fb54e453a18289916
126MANGESH <mangeshjadhav126@gmail.com>
2025-11-17 18:38:24 +0530

    app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return "Hello from Azure Flask!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
