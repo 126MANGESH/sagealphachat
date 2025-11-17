0971422991484a1355eb2058e7669e7279929c29
126MANGESH <mangeshjadhav126@gmail.com>
2025-11-17 19:07:08 +0530

    app

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Renders templates/index.html
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Use 0.0.0.0 and port 8000 for Azure
     app.run(host="0.0.0.0", port=8000)
