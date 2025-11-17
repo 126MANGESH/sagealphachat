e7b52b602a1fbde629fc7f88e53c36d26d2ae6c3
126MANGESH <mangeshjadhav126@gmail.com>
2025-11-17 18:44:36 +0530

    app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure Flask!"
+    # Renders templates/index.html
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    # Use 0.0.0.0 and port 8000 for Azure
     app.run(host="0.0.0.0", port=8000)
